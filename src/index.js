4820c0c2c01b1e288f8a7e740ca4861b47ca1092
Justin Force <justin.force@gmail.com>
2018-08-31 19:06:56 -0700

    Add @flow annotation to index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
+// @flow
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
+import { ROOT } from "./constants.json";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = document.getElementById("root");
+if (!root) {
+  throw Error(`Couldn't find DOM node with ID ${ROOT}`);
+}
+
+type Foo = { bar: boolean };
+
+const handleFoo = ({ bar }: Foo): boolean => !bar;
+
+if (handleFoo({ bar: true })) {
+  throw Error("True is false.");
+}
+
+ReactDOM.render(<App />, root);
 registerServiceWorker();
