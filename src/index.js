484bf8f451582f937c8e2193e7c65262fcf9dabe
Justin Force <justin.force@gmail.com>
2018-08-31 23:53:02 -0700

    Integrate PIXI and draw as static sprite

--- src/index.js
+++ src/index.js
@@ -1,23 +1,46 @@
 // @flow
 import React from "react";
-import ReactDOM from "react-dom";
+import { render } from "react-dom";
+import { Sprite, Stage } from "react-pixi-fiber";
+import * as PIXI from "pixi.js";
+import texture from "./unicorn.png";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import { ROOT } from "./constants.json";
 
-const root = document.getElementById("root");
-if (!root) {
-  throw Error(`Couldn't find DOM node with ID ${ROOT}`);
-}
-
+// Demonstrate Flow
 type Foo = { bar: boolean };
-
 const handleFoo = ({ bar }: Foo): boolean => !bar;
-
 if (handleFoo({ bar: true })) {
   throw Error("True is false.");
 }
 
-ReactDOM.render(<App />, root);
+// Demonstrate PIXI
+const Unicorn = props => (
+  <Sprite
+    anchor={{ x: 0.5, y: 0.5 }}
+    texture={PIXI.Texture.fromImage(texture)}
+    {...props}
+  />
+);
+const [width, height] = [800, 600];
+const stageOptions = { backgroundColor: 0x10bb99 };
+const stage = (
+  <React.Fragment>
+    <Stage width={width} height={height} options={stageOptions}>
+      <Unicorn x={width / 2} y={height / 2} scale={0.15} />
+    </Stage>
+    <App />
+  </React.Fragment>
+);
+
+// Render the app
+const root = document.getElementById("root");
+if (!root) {
+  throw Error(`Couldn't find DOM node with ID ${ROOT}`);
+}
+render(stage, root);
+
+// Serve assets from local cache in production
 registerServiceWorker();
